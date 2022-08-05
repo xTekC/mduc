243181665a169d2d023083b9d1e299ec2eff199e
xTeKc <bcd3v@pm.me>
2022-08-05 17:18:34 -0400

    get args from toml

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 
-fn version() -> String {
-    String::from("0.1.0")
-}
+
+
 
 fn info() {
-    let v = version();
-    println!("markdup, a markdown to html compiler, written by xTeKc");
-    println!("version {}", v);
+    let name = env!("CARGO_PKG_NAME");
+    let version = env!("CARGO_PKG_VERSION");
+    let description = env!("CARGO_PKG_DESCRIPTION");
+    let author = env!("CARGO_PKG_AUTHORS");
+    println!("{}, {}. Written by {}", name, description, author);
+    println!("Version {}", version);
 }
 
 fn main() {
