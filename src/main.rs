164a0e5c32409765872cfd32827b02330feebfa7
xTeKc <bcd3v@pm.me>
2022-08-05 16:16:22 -0400

    return String

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
+fn version() -> String {
+    String::from("0.1.0")
+}
+
+fn info() {
+    let v = version();
+    println!("markdup, a markdown to html compiler, written by xTeKc");
+    println!("version {}", v);
+}
+
 fn main() {
-    println!("Hello, world!");
+    info();
 }
