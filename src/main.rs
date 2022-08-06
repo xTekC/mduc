eae7cc9c818e656bcc4f688cb42d87bc0c428a9f
xTeKc <bcd3v@pm.me>
2022-08-06 00:02:15 -0400

    organize toml arg calls

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
-
-
-
-
-fn info() {
-    let name = env!("CARGO_PKG_NAME");
-    let version = env!("CARGO_PKG_VERSION");
-    let description = env!("CARGO_PKG_DESCRIPTION");
-    let author = env!("CARGO_PKG_AUTHORS");
-    println!("{} ({}), {}.", name, version, description);
-    print!("Written by {}", author);
+fn title() -> String {
+    let mut title = String::from(env!("CARGO_PKG_NAME"));
+    title.push_str(" (v");
+    title.push_str(env!("CARGO_PKG_VERSION"));
+    title.push_str("). ");
+    title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+    title
 }
 
+// called when a md file is passed in terminal
+fn parse_md_file() {}
+
+// output: title, version and description
+fn short_banner() {
+    println!("{}. ", title());
+}
+
+// output: short_banner(), author and usage
+fn long_banner() {
+    short_banner();
+    let mut author = String::from("Written by: ");
+    author.push_str(env!("CARGO_PKG_AUTHORS"));
+    let mut homepage = String::from("Homepage: ");
+    homepage.push_str(env!("CARGO_PKG_HOMEPAGE"));
+    let /*mut*/ usage = String::from("Usage: markdup <usage.md>");
+    println!("{}", author);
+    println!("{}", homepage);
+    println!("{}", usage);
+}
+
+fn info() {}
+
 fn main() {
-    info();
+    //short_banner();
+    //long_banner();
 }
